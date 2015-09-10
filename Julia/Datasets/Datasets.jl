452fc22af7e6dd4002f4f000f444a892d17ed137
Luciano Lorenti <lucianolorenti@gmail.com>
2015-09-10 08:48:58 -0300

    Cambiamos el tipo de los datasets, son matrices con los ejemplos por columna en lugar de dataframes

--- Julia/Datasets/Datasets.jl
+++ Julia/Datasets/Datasets.jl
@@ -3,11 +3,21 @@ module Datasets
 using DataFrames
 
 function skinpoints()
-  readtable("skin_points.csv",separator=',',header=false)
+  data=readtable("skin_points.csv",separator=',',header=false)
+  x=convert(Matrix,data)
+  x'
 end
 
 function iris()
-  readtable("Iris.csv",separator=';',header=true)
+  data=readtable("Iris.csv",separator=';',header=true)
+  x=convert(Matrix,data[:,1:4])
+  y_labels=data[:,5]
+  labels=unique(y_labels)
+  y=zeros(length(y_labels))
+  for i=1:length(labels)
+    y[y_labels.==labels[i]]=i
+  end
+  x',round(Int32,y),labels
 end
 
 function aprobacion()
