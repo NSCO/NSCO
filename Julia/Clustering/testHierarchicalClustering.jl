fd0698c0007ba11749846c5710bfb1b0b48ac02b
Luciano Lorenti <lucianolorenti@gmail.com>
2015-09-10 13:51:09 -0300

    Le tuve que agregar que el backend sea qt sino no me andaba

--- Julia/Clustering/testHierarchicalClustering.jl
+++ Julia/Clustering/testHierarchicalClustering.jl
@@ -8,8 +8,11 @@ m = Datasets.skinpoints()
 m = m[:,1:100:end]
 clusters=build_model(m, single_linkage_configuration(euclidean_dist,4))
 
+using PyCall
+pygui(:qt)
 using PyPlot
-colors=['red','yellow','blue','green']
+
+colors=["red","yellow","blue","green"]
 for i=1:length(clusters)
 scatter(x=m[1,clusters[i]], y=m[2,clusters[i]],color=colors[i])
 title("Iris")
