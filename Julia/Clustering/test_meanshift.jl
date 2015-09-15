46a8685821b77dcd08f4bf5f18d990981e76926b
María Emilia <maria.emilia.charnelli@gmail.com>
2015-09-14 22:54:11 -0300

    implementacion del meanshift en julia

---
+++ Julia/Clustering/test_meanshift.jl
@@ -0,0 +1,24 @@
+
+push!(LOAD_PATH, joinpath(dirname(@__FILE__),"../Datasets"))
+push!(LOAD_PATH, dirname(@__FILE__))
+
+
+using Datasets
+using MeanShift
+
+using PyCall
+pygui(:qt)
+using PyPlot
+data=Datasets.skinpoints()
+data=data[:,1:100:end]
+data=convert(Matrix{Float64},data)
+row_sum = maximum(data,2)
+data    = data ./ row_sum
+
+
+c=MeanShift.MeanShiftConfig(1.0,0.002,gaussian_window)
+
+println("Running algorithm...")
+
+clusters=MeanShift.build_model(data,c)
+
