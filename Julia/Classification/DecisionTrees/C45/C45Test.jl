bf5deae07a1d63f968b9e06587a4a84e7e2e228b
geraq <geraq1988@gmail.com>
2015-10-13 14:36:32 -0300

    PyPlot import bug corrected

--- Julia/Classification/DecisionTrees/C45/C45Test.jl
+++ Julia/Classification/DecisionTrees/C45/C45Test.jl
@@ -13,11 +13,48 @@ cu = ClassificationUtils
 (data, labels, labelNames) = Datasets.iris()
 attributeIsNumeric = collect(repeated(true,size(data,1)))
 attrNames = ["Sepal-Length", "Sepal-Width", "Petal-Length","Petal-Width"]
-confidenceLevel = 0.1
+confidenceLevel = 0.9
 conf = C45.C45Config(2, 20, attributeIsNumeric, attrNames, labelNames, confidenceLevel)
-model = C45.build_model(data, labels, conf)
-C45.pretty_print(model)
+#model = C45.build_model(data, labels, conf)
+#C45.pretty_print(model)
 
-predictions = C45.classify(data, model, conf)
+#predictions = C45.classify(data, model, conf)
+#=
 conf_matrix = cu.confusionmatrix(labels, predictions)
 println(conf_matrix)
+
+range = 0.05:0.05:0.9
+sizes = zeros(length(range))
+for i = 1:length(range)
+  println("Building tree with confidence $(range[i])")
+  conf = C45.C45Config(2, 20, attributeIsNumeric, attrNames, labelNames, range[i])
+  model = C45.build_model(data, labels, conf)
+  sizes[i] = C45.treeSize(model)
+end
+using PyPlot
+plot(range, sizes)
+=#
+
+using PyPlot
+"Algorithm Finished"
+
+figure()
+n=10
+parent=[0,1,45].*n
+children=Vector{Int}[[0,0,43].*n,[0,1,2].*n]
+
+#parent=[9,5].*n
+#children=Vector{Int}[[3,2].*n, [2,0].*n, [4,3].*n]
+c = 0.05:0.01:0.9
+parent_errors=[]
+children_errors=[]
+for x in c
+  ep,ec = C45.compare_parent_and_children(parent,children,x)
+  push!(children_errors,ec)
+  push!(parent_errors,ep)
+end
+
+
+plot(c,children_errors,"r", label="Children")
+plot(c,parent_errors,"b", label="Parent")
+legend(loc="upper right",fancybox="true")
