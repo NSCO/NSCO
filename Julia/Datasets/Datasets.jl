23d5ee09450d834ac90d483094f3409216759793
Facundo Quiroga <facundoq@gmail.com>
2015-11-02 16:36:40 -0300

    Adding datasets

--- Julia/Datasets/Datasets.jl
+++ Julia/Datasets/Datasets.jl
@@ -1,4 +1,4 @@
-module Datasets
+module Datasets 
 
 using DataFrames
 
@@ -6,6 +6,14 @@ function skinpoints()
   data=readtable(joinpath(dirname(@__FILE__),"skin_points.csv"),separator=',',header=false)
   x=convert(Matrix,data)
   x'
+end
+
+function skinpoints_small_normalized()
+  x=Datasets.skinpoints()
+  x=x[:,1:20:end]
+  x=convert(Matrix{Float64},x)
+  row_sum = maximum(x,2)
+  x    = x ./ row_sum
 end
 
 function hill_valley_train()
@@ -42,6 +50,75 @@ function movie_revies()
 end
 function aprobacion()
   readtable("aprobacion.csv",separator=';',header=true)
+end
+
+function make_circles(samples::Int=100,separation_factor::Float64=.5)
+  if (separation_factor > 1 || separation_factor < 0)
+         throw(DomainError("separation_factor has to be between 0 and 1.") )
+  end
+  elements = linspace(0, 2 * pi,samples)
+  outer_circ_x = cos(elements)
+  outer_circ_y = sin(elements)
+  inner_circ_x = outer_circ_x * separation_factor
+  inner_circ_y = outer_circ_y * separation_factor
+  x = [outer_circ_x;inner_circ_x ]
+  y = [outer_circ_y;inner_circ_y ]
+  circles = [x y]'
+
+  #shuffle
+  shuffled_order=shuffle(collect(1:size(circles,2)))
+  circles[:,1:end]=circles[:,shuffled_order]
+  #if (noise!=nothing)
+ #   perturbation =  rand(noise, 2,samples)
+#    circles+=perturbation
+  #end
+  return circles
+end
+
+function make_moons(samples::Int=100,separation_factor::Float64=1.0)
+  if (separation_factor < 0)
+         throw(DomainError())
+  end
+  elements = linspace(0,  pi,samples)
+  outer_circ_x = cos(elements) * separation_factor
+  outer_circ_y = sin(elements) * separation_factor
+  inner_circ_x = separation_factor - outer_circ_x
+  inner_circ_y =  - outer_circ_y
+  x = [outer_circ_x;inner_circ_x ]
+  y = [outer_circ_y;inner_circ_y ]
+  moons = [x y]'
+
+  #shuffle
+  shuffled_order=shuffle(collect(1:size(moons,2)))
+  moons[:,1:end]=moons[:,shuffled_order]
+  #if (noise!=nothing)
+ #   perturbation =  rand(noise, 2,samples)
+#    moons+=perturbation
+  #end
+  return moons
+end
+
+
+function make_gaussian_blobs_random_centers(samples::Int=100,features::Int=2,k::Int=3,sigma::Float64=1.0)
+    centers= rand(features,k)
+    make_gaussian_blobs(samples,centers,sigma)
+end
+function make_gaussian_blobs(samples::Int,centers::Array{Float64,2},sigma::Float64=1.0)
+  if (sigma <= 0)
+         throw(DomainError("need sigma >= 0") )
+  end
+  features,k=size(centers)
+  cov=sigma*eye(features)
+  data=zeros(features,k*samples)
+  for i in 1:k
+      var = Distributions.MvNormal(centers[:,i],cov)
+      data[:, (i-1)*samples+1:i*samples] = rand(var,samples)
+  end
+
+  shuffled_order=shuffle(collect(1:size(data,2)))
+  data[:,1:end]=data[:,shuffled_order]
+
+  return data
 end
 
 end
